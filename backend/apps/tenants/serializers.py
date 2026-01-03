371cace4e31b2609f634d4990e565943ead788ae
astper <35884105+astper21@users.noreply.github.com>
2026-01-03 12:53:53 +0800

    adjusted the creator of the  tenant
    made the creator of the tenant visible to the frontend by adjusting the backend through tenant serializers

--- backend/apps/tenants/serializers.py
+++ backend/apps/tenants/serializers.py
@@ -4,11 +4,13 @@ from .models import Tenant
 
 class TenantSerializer(serializers.ModelSerializer):
     subdomain = serializers.CharField(write_only=True)
+    created_by_name = serializers.CharField(source='created_by.username', read_only=True)
+    
 
     class Meta:
         model = Tenant
-        fields = ['id', 'tenant_name','description', 'subdomain', 'email', 'phone_number', 'account_url', 'created_at', 'created_by']
-        read_only_fields = ['account_url', 'created_by']
+        fields = ['id', 'tenant_name','description', 'subdomain', 'email', 'phone_number', 'account_url', 'created_at', 'created_by_name']
+        read_only_fields = ['account_url', 'created_by_name']
     def create(self, validated_data):
         subdomain = validated_data.pop('subdomain')
         validated_data['account_url'] = f"http://{subdomain}.unifyos.com"
