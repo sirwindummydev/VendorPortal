6eaa3863dc5947aa9a33e72f3c4139f2a32a0588
sirwindummydev <sirwindummy@gmail.com>
2026-09-06 11:30:38 +1200

    venv config
    addingtional config for venv at the vendor backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 MIDDLEWARE = [
